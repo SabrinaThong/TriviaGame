deec43ab4f573f306f08bfe7a5b0a48de45a30a3
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-05-02 17:29:20 -0500

    updating files

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//var questions = ["What are the maximum amount of clubs a person can have in their bags?", "When all players are on the green, who putts first?", 
+   // "What club do you use when you need to putt?", "Who is allowed to run on the green?", "What is the maximun amount of people that can be in a group?",
+   // "What is the biggest club in a persons bag?", "What club do you always have to use a tee with?" , "After adding up everyone's score, how do you know who did the best?",
+  //  "Where should you stand when someone else is hitting his/her golfball?", "What should you do when you hit in the water on your second shot?"];
+//var answers = ["10", "14", "11", "15" ,
+//"The one who finished the previous hole last", "Whoever's closest to the hole", "The person farthest from hole", "whoever gets to their ball first",
+//"Iron", "Driver", "Fairway Driver", "Putter",
+//"The golfers", "No one", "Children", "Residents that live on golf course",
+//"4", "5", "3", "8",
+//"Irons", "Fairway Driver", "Putter", "Driver",
+//"Driver", "Putter", "Irons","Hybrid",
+//"Person with the highest score", "Person with score closest to 50", "Person with lowest score", "Person that had the most 6's",
+//"Right next to them", "Few feet behind them", "Few feet in front of them", "On the green",
+//"Cry", "Quit", "Drop at the drop zone", "Hit another shot in the same place"];
+//correctAnswers = [1,2,3,1,0,3,0,2,1,2]
+var question1 = [
+    Q1 = "What are the maximum amount of clubs a person can have in their bags?",
+    A1= ["10", "14", "11", "15"],
+    Correct = ["14"]
+   ]
+    console.log(question1)
+
